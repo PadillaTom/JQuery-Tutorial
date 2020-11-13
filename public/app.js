edf0a29da445557e5385210cc23aa13bdfb33abf
Tomas Padilla <padillatomasagustin@gmail.com>
2020-11-13 11:35:27 +0100

    Comenzando AJAAX

--- public/app.js
+++ public/app.js
@@ -111,3 +111,104 @@
 //     $('.result-phrase').css({ color: 'cyan' });
 //   });
 // });
+
+// :::::::::::::::::::: DOM MANIPULATION :::::::::::::::::::::::
+$(function main() {
+  // ---> ADD / REMOVE Classes:
+  //   $('.result-box').addClass('active');
+  //   $('.result-box').removeClass('active');
+
+  $('.box1').on('click', function () {
+    $('.result-box').addClass('active');
+  });
+  $('.box2').on('click', function () {
+    $('.result-box').removeClass('active');
+  });
+
+  //   ---> TOGGLE Classes:
+  $('.box3').on('click', function () {
+    $('.result-box').toggleClass('active');
+  });
+
+  //  ---> CLONE STUFF:
+  //   Agarra todos los elementos con la same CLASS!
+  //   $('.box4').on('click', function () {
+  //     $('.results-container').clone().appendTo('body');
+  //   });
+
+  //   $('.box4').on('click', function () {
+  //     $('.results-container:first').clone().appendTo('body');
+  //   });
+
+  //  ---> WRAP: Wrap, unWrap, WrapInner
+  //   $('.box5').on('click', function () {
+  //     $('.box1').wrap(`<div class="active"> </div>`);
+  //   });
+  //   $('.box6').on('click', function () {
+  //     $('.box1').unwrap(`.active`);
+  //   });
+
+  // ---> CHANGING ELEMENTS: Append, AppendTo, html, prepend
+
+  //   $('.box7').on('click', function () {
+  //  APPEND:
+  // $('.result-box').append(`<p>HELLO</p>`);
+  // $(`<p>HELLO</p>`).appendTo('.result-box');
+  //
+  // HTML:
+  // console.log($('.box1').html());
+  // $('.box1').html(`<div class="active">Hello World</div>`);
+  //
+  // Text(): "Element: First, Last"
+  // console.log($('p:first').text());
+  // $('p:first').text(`New TEXT`);
+  //
+  // BEFORE, AFTER: Inster, After, InsterBefore, InsetAfter
+  //
+  // REMOVE: Select by Class, or by element and className
+  // $('.box1').remove();
+  // $('div').remove('.box3');
+  //
+  // REPLACE: All, With.
+  // $('.box1').replaceWith(`<p>Replaced</p>`);
+  //
+  // ATTRIBUTES : IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // *** ATTR, REMOVEATTR, VAL ***
+  // Get ATT:
+  // console.log($('.get-att img').attr('alt'));
+  //
+  // Change ATT:
+  // $('.get-att img').attr({ alt: 'New Alt', class: 'changed-alt' });
+  //
+  // **** VALUE: Get Values form Input Fields! *****
+  // console.log($('.get-att input').val());
+  //
+  // STYLE Properties: Width, InnerWidth, Height, CSS
+  //     console.log($('.box1').width());
+  //     if ($('.box1').width() > 70) {
+  //       $('.result-box').css({ background: 'yellow' });
+  //     }
+  //   });
+  // :::::::::::::::::::: TRAVERSING DOM :::::::::::::::::::::::
+  // PARENT: Nos muestra lso PARENTS para el first P
+  //   console.log($('p:first').parent());
+  // CHILDREN: Nos muestra los 3 grandes DIV
+  //   console.log($('div:first').children());
+  //
+  // Modifying:
+  $('.box8').on('click', function () {
+    //     $('div:first').children().css({ color: 'red' });
+    //
+    //
+    // Looping FOREACH:
+    $('p').each(function () {
+      console.log(this);
+      $(this).css({
+        color: 'green',
+      });
+    });
+    $('p').first().css({
+      color: 'red',
+    });
+  });
+});
